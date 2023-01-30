a1d8f21a2e0361b5825c9fcac1dc0bffecf7c532
Danail Dichev <danaildichev@gmail.com>
2023-01-29 17:59:22 -0800

    init
    Built a multi-tab-opener with timed delay, reverse-order opens, the ability to replace itself with the first URL requested, and the ability to prepend entries with a particular protocol. Work in progress for testing URLs before opening them.

---
+++ MultiTabOpener.js
@@ -0,0 +1,341 @@
+class MultiTabOpener
+{
+    /**
+     * fn: constructor()
+     *
+     * @param elements {Object}
+     *
+     * */
+    constructor(elements)
+    {
+        this.elements = elements;
+        this.delayValue = elements.inputs.delay.val();
+        this.delayMinimum = 1000;
+        this.init()
+    }
+    // end fn: constructor()
+
+
+    /**
+     * fn: initialize()
+     *
+     * */
+    init()
+    {
+        // ------------------------------
+        // assign form actions to buttons
+
+        // clear URLs from textarea
+        this.elements.buttons.clear.click(() => this.clearURLs());
+
+        // open URLs from textarea as new tabs
+        this.elements.buttons.open.click(() => this.openTabs());
+
+        // test URLs from textarea for validity
+        this.elements.buttons.test.click(() => this.testPaths());
+
+        // toggle display of open and test buttons
+        this.elements.inputs.validityTest.click(() => this.toggleTestAndOpenButtons());
+
+        // toggle disability of delay inputs
+        this.elements.inputs.delayCheck.click(() => this.toggleDelayInputsDisability(this.elements.inputs.delayCheck));
+
+        // update delay values
+        this.elements.inputs.delay.change(() => this.updateDelayValues(this.elements.inputs.delay));
+        this.elements.inputs.delaySlider.change(() => this.updateDelayValues(this.elements.inputs.delaySlider));
+
+        // dismiss delay alert
+        this.elements.buttons.dismissLowDelay.click(() => this.dismissDelayAlert());
+
+        // fix low delay
+        this.elements.buttons.fixLowDelay.click(() => this.fixLowDelay());
+
+        // end assign form actions to buttons
+        // ----------------------------------
+    }
+    // end fn: initialize
+
+
+    /**
+     * fn: clearURLs()
+     *
+     * clear URLs from textarea
+     *
+     * */
+    clearURLs()
+    {
+        this.elements.inputs.urls.val("");
+    }
+    // end fn: clearURLs()
+
+
+    /**
+     * fn: openTabs()
+     *
+     * open URLs from textarea as new tabs
+     *
+     * */
+    openTabs()
+    {
+        let urls = this.getURLs();
+        this.newTabs(urls, this.delayValue);
+
+        // close the multi tab opener when finished
+        // let closeTab = this.elements.inputs.closeTab.is(":checked");
+
+    }
+    // end fn: openTabs()
+
+
+    /**
+     * fn: getURLs()
+     *
+     * @return urls {array}
+     *
+     * */
+    getURLs()
+    {
+        // array to hold urls
+        let urls = [];
+
+        // break up user input into an array
+        let entries = this.elements.inputs.urls.val().split("\n");
+
+        // remove any blank lines
+        for (const e of entries) { if(e !== "") { urls.push(e); } }
+
+        // prepend protocol
+        let prepend = this.elements.inputs.prepend.val();
+        if (prepend) { urls = this.prependProtocol(urls, prepend); }
+
+        // handle if entries should be reversed
+        let reverseOrder = this.elements.inputs.openReverse.is(":checked");
+        if (reverseOrder) { return urls.reverse(); }
+        else { return urls; }
+    }
+    // end fn: getURLs()
+
+
+    /**
+     * fn: prependProtocol()
+     *
+     * @param urls {array}
+     * @param rewrite {array}
+     *
+     * */
+    prependProtocol(urls, rewrite)
+    {
+        let newURLs = [];
+        for (const u of urls) { newURLs.push(rewrite + "://" + u); }
+        return newURLs;
+    }
+    // end fn: prependProtocol()
+
+
+    /**
+     * fn: newTabs()
+     *
+     * */
+    newTabs(urls, delay)
+    {
+        // check for flag to replace the multi tab opener
+        let replaceTab = this.elements.inputs.replaceTab.is(":checked");
+
+        // open new tabs without replacing the multi tab opener
+        if (!replaceTab)
+        {
+            for (let i = 0; i < urls.length; i++)
+            {
+                if (i === 0) { this.newTab(urls[i]); }
+                else { this.newTabWithDelay(urls[i], i, delay); }
+            }
+        }
+
+        // or open all new tabs except the first
+        // and replace the multi tab opener with the first url in the array
+        else
+        {
+            // push a copy of the first url to the end of urls array
+            urls[urls.length] = urls[0];
+
+            // vars to save some compute time
+            let len = urls.length;
+            let upToLast = len - 1;
+
+            // skip the original first url and then
+            for (let i = 1; i < len; i++)
+            {
+                // open the 1th url
+                if (i === 1) { this.newTab(urls[i]); }
+
+                // delay open the other urls which are not the last one in the array
+                else if (i < upToLast) { this.newTabWithDelay(urls[i], i, delay); }
+
+                // replace the current window with
+                else if (i === upToLast) { setTimeout(() => { location = urls[i];}, i * delay); }
+            }
+        }
+    }
+    // end fn: newTabs()
+
+    /**
+     * fn: newTab()
+     *
+     * @param url {string}
+     *
+     * */
+    newTab(url)
+    {
+        //console.log("time to open " + url);
+        window.open(url, "_blank");
+    }
+    // end fn: newTab()
+
+
+    /**
+     * fn: newTabWithDelay()
+     *
+     * @param url {string}
+     * @param i {int}
+     * @param delay {int}
+     *
+     * */
+    newTabWithDelay(url, i, delay)
+    {
+        setTimeout(() => this.newTab(url), i * delay);
+    }
+    // end fn: newTabWithDelay()
+
+
+    /**
+     * fn: testPaths()
+     *
+     * test URLs from textarea for validity
+     *
+     * */
+    testPaths()
+    {
+        const testBed = document.getElementById("testBed");
+        testBed.addEventListener('shown.bs.collapse', event => {
+            $("#devMessage").toggleClass("show");
+        });
+        testBed.addEventListener('hide.bs.collapse', event => {
+            $("#devMessage").toggleClass("show");
+        });
+    }
+    // end fn: testPaths()
+
+
+    /**
+     * fn: toggleTestAndOpenButtons()
+     *
+     * toggle display of open and test buttons
+     *
+     * */
+    toggleTestAndOpenButtons()
+    {
+        this.elements.buttons.test.toggleClass("d-none");
+        this.elements.buttons.open.toggleClass("d-none");
+    }
+    // end fn: toggleTestAndOpenButtons()
+
+
+    /**
+     * fn: toggleDelayInputsDisability()
+     *
+     * @param checkbox {Object}
+     *
+     * */
+    toggleDelayInputsDisability(checkbox)
+    {
+        let isChecked = checkbox.is(":checked");
+        if (isChecked) { this.resetDelayInputs(this.delayValue); }
+        else { this.resetDelayInputs(0) }
+    }
+    // end fn: toggleDelayInputsDisability()
+
+
+    /**
+     * fn: resetDelayInputs()
+     *
+     * */
+    resetDelayInputs(delayValue)
+    {
+        this.elements.inputs.delay.val(delayValue);
+        this.toggleDisabled(this.elements.inputs.delay);
+        this.elements.inputs.delaySlider.val(delayValue);
+        this.toggleDisabled(this.elements.inputs.delaySlider);
+        //this.toggleDelayAlert();
+        if(delayValue <= 999) { this.showDelayAlert(); }
+        else { this.dismissDelayAlert(); }
+    }
+    // end fn: resetDelayInputs()
+
+
+    /**
+     * fn: toggleDisabled()
+     *
+     * */
+    toggleDisabled(el)
+    {
+        let isDisabled = el.is(":disabled");
+        el.prop("disabled", !isDisabled);
+    }
+    // end fn: toggleDisabled()
+
+
+    /**
+     * fn: updateDelayValues()
+     *
+     * */
+    updateDelayValues(el)
+    {
+        let newVal = el.val();
+        this.elements.inputs.delay.val(newVal);
+        this.elements.inputs.delaySlider.val(newVal);
+        this.delayValue = newVal;
+        if(newVal <= 999) { this.showDelayAlert(); }
+        else { this.dismissDelayAlert(); }
+    }
+    // end fn: updateDelayValues()
+
+
+    /**
+     * fn: clearDelayAlert()
+     *
+     * */
+    dismissDelayAlert()
+    {
+        this.elements.alerts.lowDelay.addClass("d-none");
+    }
+    // end fn: clearDelayAlert()
+
+
+    /**
+     * fn: showDelayAlert()
+     *
+     * */
+    showDelayAlert()
+    {
+        this.elements.alerts.lowDelay.removeClass("d-none");
+    }
+    // end fn: showDelayAlert()
+
+
+    /**
+     * fn: fixLowDelay()
+     *
+     * */
+    fixLowDelay()
+    {
+        this.elements.inputs.delay.val(this.delayMinimum);
+        this.elements.inputs.delay.prop("disabled", false);
+        this.elements.inputs.delaySlider.val(this.delayMinimum);
+        this.elements.inputs.delaySlider.prop("disabled", false);
+        this.elements.inputs.delayCheck.prop("checked", true);
+        this.delayValue = this.delayMinimum;
+        this.dismissDelayAlert();
+    }
+    // end fn: fixLowDelay()
+
+}
---
+++ index.php
@@ -0,0 +1,237 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index</title>
+
+    <!-------------------->
+    <!-- CSS: Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <!----------------------->
+    <!-- CSS: Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body class="text-bg-dark">
+
+<!------------------>
+<!-- Page Content -->
+
+<main>
+
+    <!-- Title Bar -->
+    <section class="container my-3">
+        <div class="row">
+            <div class="col text-center">
+                <h1>Multi Tab Opener</h1>
+            </div>
+        </div>
+    </section>
+    <!-- end Title Bar -->
+
+    <!-- Input Panel -->
+    <section class="container py-3 bg-secondary rounded">
+
+        <!-- form: urls to be opened -->
+        <div class="row g-3">
+
+            <!-- Column: URLs input -->
+            <div class="col-md-12 col-lg-8">
+
+                <h2>URLs</h2>
+
+                    <!-- textarea: urls -->
+                    <div class="mb-3">
+                        <label for="input-urls" class="form-label">Copy-Paste or type each web address onto a separate line. Blank lines will be ignored.</label>
+                        <textarea class="form-control" id="input-urls" rows="12"></textarea>
+                        <!--<div id="help-input-urls" class="form-text text-white">*Your browser will try to block this the first time you use this form.</div>-->
+                    </div>
+                    <!-- textarea: urls -->
+
+            </div>
+            <!-- end Column: URLs input -->
+
+            <!-- Options -->
+            <div class="col-md-12 col-lg-4">
+
+                <h2>Options</h2>
+
+                <!-- delay -->
+                <div id="help-input-delay" class="form-text text-white d-block">
+                    <label for="input-delay" class="form-label">Millisecond delay before each new tab:</label>
+                </div>
+                <!-- input group: delay -->
+                <div class="input-group mb-3">
+                    <div class="input-group-text">
+                        <input type="checkbox" id="check-delay" class="form-check-input mt-0" aria-label="Checkbox for delay" checked>
+                    </div>
+                    <input type="number" id="input-delay" class="form-control" value="2000" aria-label="Text input with checkbox for delay">
+
+                    <input type="range" class="form-range" min="1" max="10000" id="slider-delay" value="2000">
+
+                </div>
+                <!-- end input group: delay -->
+                <!-- alert: low delay -->
+                <div id="alert-lowDelay" class="alert alert-warning alert-dismissible fade show d-none" role="alert">
+                    <p class="lead"><strong><i class="fa-solid fa-triangle-exclamation me-2"></i> Warning</strong></p>
+                    <p>This operation can be <u>very</u> resource intensive.<br>You should use at least a 1-second delay.</p>
+                    <button id="btn-fixLowDelay" class="btn btn-warning" type="button">Fix</button>
+                    <button type="button" id="dismiss-lowDelay" class="btn-close" aria-label="Dismiss Low Delay alert"></button>
+                </div>
+                <!-- end alert: low delay -->
+                <!-- end delay -->
+
+                <!-- overwrite -->
+                <div id="help-input-overwrite" class="form-text text-white d-block">
+                    <label for="input-delay" class="form-label">Prepend entries with:</label>
+                </div>
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="input-overwrite">Protocol</label>
+                    <select class="form-select" id="input-overwrite">
+                        <option value="" selected></option>
+                        <option value="https">https</option>
+                        <option value="http">http</option>
+                    </select>
+                </div>
+                <!-- end overwrite -->
+
+                <!-- test valid path -->
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="input-testValidPath">
+                    <label class="form-check-label" for="input-testValidPath">Test for reachability before opening</label>
+                </div>
+                <!-- end test valid path -->
+
+                <!-- close this tab -->
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="input-replaceThisTab">
+                    <label class="form-check-label" for="input-replaceThisTab">Replace this tab with first entry</label>
+                </div>
+                <!-- end close this tab -->
+
+                <!-- close this tab -->
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="input-openReverse">
+                    <label class="form-check-label" for="input-openReverse">Open in reverse order</label>
+                </div>
+                <!-- end close this tab -->
+
+                <!-- buttons -->
+                <div class="mt-4 text-center">
+                    <div class="btn-group" role="group" aria-label="Form action">
+
+                        <!-- btn: test -->
+                        <button id="btn-testURLs" type="button" class="btn btn-primary d-none"
+                                data-bs-toggle="collapse" data-bs-target="#testBed"
+                                aria-expanded="false" aria-controls="testBed">
+                            <i class="fa-solid fa-file-lines me-2"></i>
+                            Test
+                        </button>
+
+                        <!-- btn: open -->
+                        <button id="btn-openTabs" type="button" class="btn btn-success rounded-start">
+                            <i class="fa-solid fa-up-right-from-square me-2"></i>
+                            Open
+                        </button>
+
+                        <!-- btn: clear -->
+                        <button id="btn-clearURLs" type="button" class="btn btn-dark">
+                            <i class="fa-solid fa-delete-left me-2"></i>
+                            Clear
+                        </button>
+
+                    </div>
+                </div>
+                <!-- end buttons -->
+
+            </div>
+            <!-- end Options -->
+
+        </div>
+        <!-- end form: urls to be opened -->
+
+    </section>
+    <!-- end Input Panel -->
+
+    <!-- Test Bed -->
+    <div class="collapse" id="testBed">
+    <section class="container mt-3 py-3 bg-secondary rounded">
+        <div class="col">
+
+            <h2 class="text-center fade" id="devMessage">This feature is currently under development.</h2>
+
+        </div>
+    </section>
+    </div>
+    <!-- end Test Bed -->
+
+</main>
+
+<!-- end Page Content -->
+<!---------------------->
+
+
+<!------------->
+<!-- scripts -->
+
+<!---------------->
+<!-- JS: jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+
+<!------------------->
+<!-- JS: Bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+<!------------------------>
+<!-- JS: MultiTabOpener -->
+<script src="MultiTabOpener.js"></script>
+<script>
+
+    $(document).ready(function() {
+
+        // get form elements
+        const elements =
+        {
+            "inputs":
+            {
+                "urls": $("#input-urls"),
+                "delayCheck": $("#check-delay"),
+                "delay": $("#input-delay"),
+                "delaySlider": $("#slider-delay"),
+                "prepend": $("#input-overwrite"),
+                "validityTest": $("#input-testValidPath"),
+                "replaceTab": $("#input-replaceThisTab"),
+                "openReverse": $("#input-openReverse"),
+            },
+
+            "alerts":
+            {
+                "lowDelay": $("#alert-lowDelay"),
+            },
+
+            "buttons":
+            {
+                "test": $("#btn-testURLs"),
+                "open": $("#btn-openTabs"),
+                "clear": $("#btn-clearURLs"),
+                "fixLowDelay": $("#btn-fixLowDelay"),
+                "dismissLowDelay": $("#dismiss-lowDelay")
+            }
+        }
+
+        const mto = new MultiTabOpener(elements);
+
+    });
+
+</script>
+<!-- end JS: MultiTabOpener -->
+
+<!-- end scripts -->
+<!----------------->
+
+</body>
+</html>
